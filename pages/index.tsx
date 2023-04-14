626e1f5bae254af372a11aa1d67d7f7a7e52430d
Jaebum Lee <jaebumlee@Jaebums-MacBook-Pro.local>
2023-04-14 14:26:20 -0400

    login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { Button, Flex, Heading, Input, useColorMode, useColorModeValue } from '@chakra-ui/react'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  const { toggleColorMode } = useColorMode()
+  const formBackground = useColorModeValue("gray.100", "gray.700")
+  return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" background={formBackground} p={12} rounded={6}>
+        <Heading mb={6}>Log in</Heading>
+        <Input placeholder="example@email.com" variant="filled" mb={3} type="email" />
+        <Input placeholder="********" variant="filled" mb={6} typer="password" />
+        <Button mb={6} colorScheme="teal">Log in</Button>
+        <Button onClick={toggleColorMode}>Toggle Color</Button>
+      </Flex>
+    </Flex>
+  )
+}
 
 export default IndexPage
